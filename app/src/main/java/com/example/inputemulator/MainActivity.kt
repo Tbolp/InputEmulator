42c1c1cfdd14e69dc25d4a9c54094c5604aa8306
Tbolp <tbolp@qq.com>
2025-10-21 20:01:11 +0800

    develop

--- app/src/main/java/com/example/inputemulator/MainActivity.kt
+++ app/src/main/java/com/example/inputemulator/MainActivity.kt
@@ -4,18 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -64,5 +76,23 @@ fun HomePage(navController: NavController) {
 
 @Composable
 fun TouchSettingPage() {
-    Text("second")
+    var sliderValue by remember {
+        mutableStateOf(1f)
+    }
+    Column {
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Text("Sensitive")
+            Slider(
+                value = sliderValue,
+                onValueChange = { value -> sliderValue = value},
+                valueRange = 1f..10f,
+                thumb = {
+                    SliderDefaults.Thumb()
+                }
+            )
+        }
+        Button(onClick = {}) {
+            Text("Start")
+        }
+    }
 }
