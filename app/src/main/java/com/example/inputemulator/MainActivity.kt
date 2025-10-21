da0406d477422dbc953eb3685705a21d29076556
tbolp <tbolp@qq.com>
2025-10-22 01:31:33 +0800

    develop

--- app/src/main/java/com/example/inputemulator/MainActivity.kt
+++ app/src/main/java/com/example/inputemulator/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,10 +22,12 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -36,6 +40,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.createGraph
 import com.example.inputemulator.ui.theme.InputEmulatorTheme
+import java.time.temporal.ValueRange
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -45,7 +50,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             InputEmulatorTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize().padding(10.dp)) { paddingValues ->
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(10.dp)
+                ) { paddingValues ->
                     val navController = rememberNavController()
                     val navGraph = remember {
                         navController.createGraph("Home") {
@@ -55,7 +64,11 @@ class MainActivity : ComponentActivity() {
                             composable("TouchSettingPage") { TouchSettingPage() }
                         }
                     }
-                    NavHost(navController = navController, graph = navGraph, modifier = Modifier.padding(paddingValues))
+                    NavHost(
+                        navController = navController,
+                        graph = navGraph,
+                        modifier = Modifier.padding(paddingValues)
+                    )
                 }
             }
         }
@@ -76,23 +89,52 @@ fun HomePage(navController: NavController) {
 
 @Composable
 fun TouchSettingPage() {
-    var sliderValue by remember {
-        mutableStateOf(1f)
+    var sensitive by remember {
+        mutableFloatStateOf(1f)
+    }
+    var landscape by remember {
+        mutableStateOf(false)
     }
     Column {
-        Row (verticalAlignment = Alignment.CenterVertically){
-            Text("Sensitive")
-            Slider(
-                value = sliderValue,
-                onValueChange = { value -> sliderValue = value},
-                valueRange = 1f..10f,
-                thumb = {
-                    SliderDefaults.Thumb()
-                }
-            )
-        }
-        Button(onClick = {}) {
-            Text("Start")
-        }
+        FormSlider(
+            name = "sensitiv",
+            value = sensitive,
+            onValueChange = { sensitive = it },
+            valueRange = 0f..10f
+        )
+        FormSwitch(name = "landscape", value = landscape, onValueChange = { landscape = it })
+        Button(onClick = {}) { Text("start") }
     }
 }
+
+@Composable
+fun FormSlider(
+    name: String,
+    value: Float,
+    onValueChange: (Float) -> Unit,
+    valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(name)
+        Spacer(modifier = Modifier.width(10.dp))
+        Slider(
+            value = value,
+            onValueChange = onValueChange,
+            valueRange = valueRange,
+        )
+    }
+}
+
+@Composable
+fun FormSwitch(
+    name: String,
+    value: Boolean,
+    onValueChange: ((Boolean) -> Unit)?,
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(name)
+        Spacer(modifier = Modifier.weight(1f))
+        Switch(checked = value, onCheckedChange = onValueChange)
+    }
+}
+
